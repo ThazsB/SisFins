dba84e73c721b191d9281819ea7080f3ab7145a4
Thalles <thallesbennerytb@gmail.com>
2026-02-13 02:00:39 -0300

    Updates

--- assets/index-DnRywrJf.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DtNvJ8FT.js","./index-CNHTL47a.js"])))=>i.map(i=>d[i]);
-import{_ as r}from"./index-BSOZQh7F.js";import{registerPlugin as t}from"./index-CNHTL47a.js";const p=t("SplashScreen",{web:()=>r(()=>import("./web-DtNvJ8FT.js"),__vite__mapDeps([0,1]),import.meta.url).then(e=>new e.SplashScreenWeb)});export{p as SplashScreen};
